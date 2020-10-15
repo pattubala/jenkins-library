2079e001f7ee70de97b9fb6ad97bdc80d799ee09
pattubala <pattubalalokesh1073@gmail.com>
2020-10-15 11:46:33 +0530

    Update codeScan.groovy

--- vars/codeScan.groovy
+++ vars/codeScan.groovy
@@ -1,5 +1,6 @@
-def call {
+#!/usr/bin/env groovy
+def call(body) {
     withSonarQubeEnv('Sonarqube_7.6') {
-        sh "mvn sonar:sonar -Dsonar.projectName= ${SONAR_PROJECT_NAME} -Dsonar.projectKey=stageParams.sonar_project_key -Dsonar.java.binaries=stageParams.sonar_java_binaries -Dsonar.language=stageParams.sonar_language -Dsonar.sourceEncoding=UTF-8"
+        sh "mvn sonar:sonar -Dsonar.projectName= ${SONAR_PROJECT_NAME} -Dsonar.projectKey=${SONAR_PROJECT_KEY} -Dsonar.java.binaries=${SONAR_JAVA_BINARIES} -Dsonar.language=${SONAR_LANGUAGE} -Dsonar.sourceEncoding=UTF-8"
    } 
 }
