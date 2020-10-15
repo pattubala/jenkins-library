8f741b8db13f48b4753b4625e02577ab15017971
pattubala <pattubalalokesh1073@gmail.com>
2020-10-15 11:39:40 +0530

    Update codeScan.groovy

--- vars/codeScan.groovy
+++ vars/codeScan.groovy
@@ -1,5 +1,5 @@
 def call(Map stageParams) {
     withSonarQubeEnv('Sonarqube_7.6') {
-       sh "mvn sonar:sonar -Dsonar.projectName=stageParams.sonar_project_name -Dsonar.projectKey=stageParams.sonar_project_key -Dsonar.java.binaries=stageParams.sonar_java_binaries -Dsonar.language=stageParams.sonar_language -Dsonar.sourceEncoding=UTF-8"
+       sh "mvn sonar:sonar -Dsonar.projectName= [[sonar_project_name: stageParams.sonar_project_name ]] -Dsonar.projectKey=stageParams.sonar_project_key -Dsonar.java.binaries=stageParams.sonar_java_binaries -Dsonar.language=stageParams.sonar_language -Dsonar.sourceEncoding=UTF-8"
    } 
 }
