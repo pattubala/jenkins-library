10ab331d8c553bd4eff07095990bbf703fe90a70
pattubala <pattubalalokesh1073@gmail.com>
2020-11-09 11:48:44 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,14 @@
-def call(Map stageParams) {
- 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
-  }
+#!/usr/bin/env groovy
+def call(Map args=[:], Closure body={}) {
+    node {
+	    stage ('SCM Checkout') {
+            dir("${args.PROJECT_WORKSPACE_PATH}"){
+                git (url: "${args.GITHUB_CLONE_URL}",
+                    branch: "${args.BRANCH_NAME}",
+                    credentialsId: 'Github')
+            }
+        }
+		}
+        body()
+    }
+}
