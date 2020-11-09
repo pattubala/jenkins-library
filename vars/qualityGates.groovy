3c8b65c963738ed4c96c43992308f59ff729d4bd
pattubala <pattubalalokesh1073@gmail.com>
2020-11-09 11:50:15 +0530

    Update qualityGates.groovy

--- vars/qualityGates.groovy
+++ vars/qualityGates.groovy
@@ -1,14 +1,18 @@
 #!/usr/bin/env groovy
 def call(Map args=[:], Closure body={}) {
     node {
-        stage("SONARQUBE STATIC CODE ANALYSIS") {
+        stage("QUALITY GATES CHECK") {
                 dir("${args.PROJECT_WORKSPACE_PATH}"){
-				    script {
-                        withSonarQubeEnv('Sonarqube_7.6') {
-                            sh "mvn sonar:sonar -Dsonar.projectName=${args.SONAR_PROJECT_NAME} -Dsonar.projectKey=${args.SONAR_PROJECT_KEY} -Dsonar.java.binaries=${args.SONAR_JAVA_BINARIES} -Dsonar.language=${args.SONAR_LANGUAGE} -Dsonar.sourceEncoding=UTF-8"
-                        }	
-						
-                }
+                    timeout(time: 3, unit: 'MINUTES') {
+                        echo "Initializing quality gates..."
+                        sh 'sleep 10' //small delay because project quality can still being published on previous stage (specially on bigger projects).  
+                        def result = waitForQualityGate() //this is enabled by quality gates plugin: https://wiki.jenkins.io/display/JENKINS/Quality+Gates+Plugin
+                        if (result.status != 'OK') {
+                             error "Pipeline aborted due to quality gate failure: ${result.status}"
+                        } else {
+                             echo "Quality gate passed with result: ${result.status}"
+                        }
+                    } 
             }
         }
 		}
