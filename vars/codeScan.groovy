4ec37dd79f40a45bdd89cfd6683ee0b07d1f3eaf
pattubala <pattubalalokesh1073@gmail.com>
2020-11-09 13:57:06 +0530

    Delete codeScan.groovy

--- vars/codeScan.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-def call(Map args) {
-    withSonarQubeEnv('Sonarqube_7.6') {
-        sh "mvn sonar:sonar -Dsonar.projectName=${args.projectname} -Dsonar.projectKey=${args.projectkey} -Dsonar.java.binaries=${args.javabinaries} -Dsonar.language=${args.language} -Dsonar.sourceEncoding=UTF-8"
-   } 
-}
