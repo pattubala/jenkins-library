32b60a13e7a89f54d85d31e3147c8be89381441a
pattubala <pattubalalokesh1073@gmail.com>
2020-11-09 11:50:53 +0530

    Update mavenBuild.groovy

---
+++ vars/mavenBuild.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+def call(Map args=[:], Closure body={}) {
+    node {
+		stage ('Maven Build') {
+                dir("${args.PROJECT_WORKSPACE_PATH}"){
+                    script {
+                      sh "mvn clean install"
+				}
+		    }
+		}
+		}
+        body()
+    }
+}
